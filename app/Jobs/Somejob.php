1452138be0d79a4f5020cf758ff3e7c8585c9fcc
imsat <satyajit.cse@yahoo.com>
2024-03-29 20:56:41 +0600

    horizon practice

---
+++ app/Jobs/Somejob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Batchable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class Somejob implements ShouldQueue
+{
+    use Batchable, Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $user;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        //
+    }
+
+    public function tags()
+    {
+        return ['somejob', 'user: '. $this->user->id];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Mail\OrderShipped;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redis;
 
 Route::get('/', function () {
+//    Redis::set('rtr', '4v');
+//    $value = Redis::get('rtr');
+//    dd($value);
     return view('welcome');
+});
+
+Route::post('/', function () {
+    Mail::to('satyajit.cse@yahoo.com')->send(new OrderShipped());
+    return redirect('/');
 });
